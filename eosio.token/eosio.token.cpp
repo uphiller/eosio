7169fbfd8f78eccad53c88227b7d7eb75681f4e4
aiden <aiden@hexlant.com>
2018-10-22 20:57:12 +0900

    추가

--- eosio.token/eosio.token.cpp
+++ eosio.token/eosio.token.cpp
@@ -7,30 +7,20 @@
 
 namespace eosio {
 
-void token::create( account_name issuer,
-                    asset        maximum_supply )
+void token::create( name   issuer,
+                    asset  maximum_supply )
 {
-    // action을 푸시한 게정이 _self(컨트랙트 계정) 인지 확인
     require_auth( _self );
 
-    // 파라메터로 들어온 에셋에 대한 심볼 추출
     auto sym = maximum_supply.symbol;
-
-    // 심볼이 올바른 심볼인지 확인 
     eosio_assert( sym.is_valid(), "invalid symbol name" );
-    // Asset에 대한 내용이 올바른지 확인
     eosio_assert( maximum_supply.is_valid(), "invalid supply");
-    // 최대 발행량이 0보다 큰지 확인
     eosio_assert( maximum_supply.amount > 0, "max-supply must be positive");
 
-    // 기존의 stat table 읽기
-    stats statstable( _self, sym.name() );
-
-    // 동일한 심볼을 갖는 stst이 있는지 확인
-    auto existing = statstable.find( sym.name() );
+    stats statstable( _self, sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
     eosio_assert( existing == statstable.end(), "token with symbol already exists" );
 
-    // 새로운 stat정보 저장
     statstable.emplace( _self, [&]( auto& s ) {
        s.supply.symbol = maximum_supply.symbol;
        s.max_supply    = maximum_supply;
@@ -39,64 +29,73 @@ void token::create( account_name issuer,
 }
 
 
-void token::issue( account_name to, asset quantity, string memo )
+void token::issue( name to, asset quantity, string memo )
 {
-    // 심볼에 대한 체크
     auto sym = quantity.symbol;
     eosio_assert( sym.is_valid(), "invalid symbol name" );
-
-    // 메모의 크기가 256 보다 작은지 확인
     eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
 
-    // 심볼을 스코프로 하여 테이블 로드
-    auto sym_name = sym.name();
-    stats statstable( _self, sym_name );
-    auto existing = statstable.find( sym_name );
-
-    // 해당 심볼을 갖는 stat정보가 있는지 확인
+    stats statstable( _self, sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
     eosio_assert( existing != statstable.end(), "token with symbol does not exist, create token before issue" );
     const auto& st = *existing;
 
-    // action을 push한 게정이 해당 토큰의 발행자 인지 체크
     require_auth( st.issuer );
-    // 발행하려는 토큰의 수량이 0보다 큰지 확인
     eosio_assert( quantity.is_valid(), "invalid quantity" );
     eosio_assert( quantity.amount > 0, "must issue positive quantity" );
-    // 발행하려는 토큰의 심볼이 stat정보과 맞는지 확인
+
     eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
-    // 발행했을때의 발행량이 최대 발행량을 넘지는 않는지 확인 
     eosio_assert( quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
 
-    // 공급량 업데이트
-    statstable.modify( st, 0, [&]( auto& s ) {
+    statstable.modify( st, same_payer, [&]( auto& s ) {
        s.supply += quantity;
     });
-    // 발행권자 게좌에 발행한 금액 추가
+
     add_balance( st.issuer, quantity, st.issuer );
 
-    // 만일 발행된 토큰을 받을 사람이 발행자가 아니라면, 토큰 전송 
     if( to != st.issuer ) {
-       SEND_INLINE_ACTION( *this, transfer, {st.issuer,N(active)}, {st.issuer, to, quantity, memo} );
+      SEND_INLINE_ACTION( *this, transfer, { {st.issuer, "active"_n} },
+                          { st.issuer, to, quantity, memo }
+      );
     }
 }
 
-void token::transfer( account_name from,
-                      account_name to,
-                      asset        quantity,
-                      string       memo )
+void token::retire( asset quantity, string memo )
 {
-    // 자기자신에게 보내는 전송이면 assert
+    auto sym = quantity.symbol;
+    eosio_assert( sym.is_valid(), "invalid symbol name" );
+    eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    stats statstable( _self, sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
+    eosio_assert( existing != statstable.end(), "token with symbol does not exist" );
+    const auto& st = *existing;
+
+    require_auth( st.issuer );
+    eosio_assert( quantity.is_valid(), "invalid quantity" );
+    eosio_assert( quantity.amount > 0, "must retire positive quantity" );
+
+    eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+
+    statstable.modify( st, same_payer, [&]( auto& s ) {
+       s.supply -= quantity;
+    });
+
+    sub_balance( st.issuer, quantity );
+}
+
+void token::transfer( name    from,
+                      name    to,
+                      asset   quantity,
+                      string  memo )
+{
     eosio_assert( from != to, "cannot transfer to self" );
-    // action을 push한 게정이 from인지 확인
     require_auth( from );
-
-    // 받을 계정이 EOS네트워크에 존재하는 계정인지 확인
     eosio_assert( is_account( to ), "to account does not exist");
-    auto sym = quantity.symbol.name();
-    stats statstable( _self, sym );
-    const auto& st = statstable.get( sym );
+    auto sym = quantity.symbol.code();
+    stats statstable( _self, sym.raw() );
+    const auto& st = statstable.get( sym.raw() );
 
-    // from과 to 계정에 action 내용 전파
     require_recipient( from );
     require_recipient( to );
 
@@ -105,40 +104,67 @@ void token::transfer( account_name from,
     eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
     eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
 
-    // from의 잔액에서 보내려는 양만큼 잔액을 감소시키고, 받는 가람 잔액을 증가시킴
+    auto payer = has_auth( to ) ? to : from;
+
     sub_balance( from, quantity );
-    add_balance( to, quantity, from );
+    add_balance( to, quantity, payer );
 }
 
-void token::sub_balance( account_name owner, asset value ) {
-   accounts from_acnts( _self, owner );
+void token::sub_balance( name owner, asset value ) {
+   accounts from_acnts( _self, owner.value );
 
-   const auto& from = from_acnts.get( value.symbol.name(), "no balance object found" );
+   const auto& from = from_acnts.get( value.symbol.code().raw(), "no balance object found" );
    eosio_assert( from.balance.amount >= value.amount, "overdrawn balance" );
 
-
-   if( from.balance.amount == value.amount ) {
-      from_acnts.erase( from );
-   } else {
-      from_acnts.modify( from, owner, [&]( auto& a ) {
-          a.balance -= value;
+   from_acnts.modify( from, owner, [&]( auto& a ) {
+         a.balance -= value;
       });
-   }
 }
 
-void token::add_balance( account_name owner, asset value, account_name ram_payer )
+void token::add_balance( name owner, asset value, name ram_payer )
 {
-   accounts to_acnts( _self, owner );
-   auto to = to_acnts.find( value.symbol.name() );
+   accounts to_acnts( _self, owner.value );
+   auto to = to_acnts.find( value.symbol.code().raw() );
    if( to == to_acnts.end() ) {
       to_acnts.emplace( ram_payer, [&]( auto& a ){
         a.balance = value;
       });
    } else {
-      to_acnts.modify( to, 0, [&]( auto& a ) {
+      to_acnts.modify( to, same_payer, [&]( auto& a ) {
         a.balance += value;
       });
    }
 }
 
+void token::open( name owner, const symbol& symbol, name ram_payer )
+{
+   require_auth( ram_payer );
+
+   auto sym_code_raw = symbol.code().raw();
+
+   stats statstable( _self, sym_code_raw );
+   const auto& st = statstable.get( sym_code_raw, "symbol does not exist" );
+   eosio_assert( st.supply.symbol == symbol, "symbol precision mismatch" );
+
+   accounts acnts( _self, owner.value );
+   auto it = acnts.find( sym_code_raw );
+   if( it == acnts.end() ) {
+      acnts.emplace( ram_payer, [&]( auto& a ){
+        a.balance = asset{0, symbol};
+      });
+   }
+}
+
+void token::close( name owner, const symbol& symbol )
+{
+   require_auth( owner );
+   accounts acnts( _self, owner.value );
+   auto it = acnts.find( symbol.code().raw() );
+   eosio_assert( it != acnts.end(), "Balance row already deleted or never existed. Action won't have any effect." );
+   eosio_assert( it->balance.amount == 0, "Cannot close because the balance is not zero." );
+   acnts.erase( it );
+}
+
 } /// namespace eosio
+
+EOSIO_DISPATCH( eosio::token, (create)(issue)(transfer)(open)(close)(retire) )
--- eosio.token/eosio.token.hpp
+++ eosio.token/eosio.token.hpp
@@ -17,80 +17,66 @@ namespace eosio {
 
    using std::string;
 
-   class token : public contract {
+   class [[eosio::contract("eosio.token")]] token : public contract {
       public:
-         token( account_name self ):contract(self){}
-      
-         // 새로운 토큰을 만드는 함수
-         void create( account_name issuer,
-                      asset        maximum_supply);
+         using contract::contract;
 
-         // 만들어진 토큰에 대해서 발행을 하는 함수 
-         void issue( account_name to, asset quantity, string memo );
-         
-         // from 계정이 가진 토큰을 to 에게 전송하는 함수
-         void transfer( account_name from,
-                        account_name to,
-                        asset        quantity,
-                        string       memo );
-      
-         // 해당 심볼을 가진 토큰의 발행량을 확인하는 함수
-         inline asset get_supply( symbol_name sym )const;
-         
-         // owner가 가진 sym 심볼을 가진 토큰의 잔액을 확인하는 함수
-         inline asset get_balance( account_name owner, symbol_name sym )const;
+         [[eosio::action]]
+         void create( name   issuer,
+                      asset  maximum_supply);
+
+         [[eosio::action]]
+         void issue( name to, asset quantity, string memo );
+
+         [[eosio::action]]
+         void retire( asset quantity, string memo );
+
+         [[eosio::action]]
+         void transfer( name    from,
+                        name    to,
+                        asset   quantity,
+                        string  memo );
+
+         [[eosio::action]]
+         void open( name owner, const symbol& symbol, name ram_payer );
+
+         [[eosio::action]]
+         void close( name owner, const symbol& symbol );
+
+         static asset get_supply( name token_contract_account, symbol_code sym_code )
+         {
+            stats statstable( token_contract_account, sym_code.raw() );
+            const auto& st = statstable.get( sym_code.raw() );
+            return st.supply;
+         }
+
+         static asset get_balance( name token_contract_account, name owner, symbol_code sym_code )
+         {
+            accounts accountstable( token_contract_account, owner.value );
+            const auto& ac = accountstable.get( sym_code.raw() );
+            return ac.balance;
+         }
 
       private:
-         // 계정별 잔액 정보를 저장하는 구조체
-         /// @abi table accounts i64
-         struct account {
+         struct [[eosio::table]] account {
             asset    balance;
 
-            uint64_t primary_key()const { return balance.symbol.name(); }
-            EOSLIB_SERIALIZE(account,(balance))
-
-         };
-         // 발행한 토큰의 상태 값
-         /// @abi table stat i64
-         struct stat {
-            asset          supply;
-            asset          max_supply;
-            account_name   issuer;
-
-            uint64_t primary_key()const { return supply.symbol.name(); }
-            EOSLIB_SERIALIZE(stat,(supply)(max_supply)(issuer))
-
+            uint64_t primary_key()const { return balance.symbol.code().raw(); }
          };
 
-         typedef eosio::multi_index<N(accounts), account> accounts;
-         typedef eosio::multi_index<N(stat), stat> stats;
-         
-         // owner가 가진 토큰의 수량 감액 / 증액
-         void sub_balance( account_name owner, asset value );
-         void add_balance( account_name owner, asset value, account_name ram_payer );
+         struct [[eosio::table]] currency_stats {
+            asset    supply;
+            asset    max_supply;
+            name     issuer;
 
-      public:
-         struct transfer_args {
-            account_name  from;
-            account_name  to;
-            asset         quantity;
-            string        memo;
+            uint64_t primary_key()const { return supply.symbol.code().raw(); }
          };
+
+         typedef eosio::multi_index< "accounts"_n, account > accounts;
+         typedef eosio::multi_index< "stat"_n, currency_stats > stats;
+
+         void sub_balance( name owner, asset value );
+         void add_balance( name owner, asset value, name ram_payer );
    };
 
-   asset token::get_supply( symbol_name sym )const
-   {
-      stats statstable( _self, sym );
-      const auto& st = statstable.get( sym );
-      return st.supply;
-   }
-
-   asset token::get_balance( account_name owner, symbol_name sym )const
-   {
-      accounts accountstable( _self, owner );
-      const auto& ac = accountstable.get( sym );
-      return ac.balance;
-   }
-
 } /// namespace eosio
-EOSIO_ABI( eosio::token, (create)(issue)(transfer) )
